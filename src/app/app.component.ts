f668131a497c7b48cce285c9b44cc5171d199a46
amrit991 <amrit.raj@doceree.com>
2020-05-21 21:26:00 +0530

    circular ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {ThemePalette} from '@angular/material/core';
+import {ProgressSpinnerMode} from '@angular/material/progress-spinner';
+
+interface platform {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'earnings-calc';
+  autoTicks = false;
+  disabled = false;
+  invert = false;
+  maxUsers = 160000;
+  maxSlots = 10;
+  min = 1;
+  showTicks = false;
+  step = 1;
+  stepUsers = 100;
+  thumbLabel = true;
+  registeredUsers = 1;
+  totalSlots = 1;
+  vertical = false;
+  tickInterval = 1;
+  selectedValue: string;
+  color: ThemePalette = 'primary';
+  mode: ProgressSpinnerMode = 'determinate';
+  circularValue = 42336/160000 * 100;
+  circularTitle = '$42336';
+
+  platforms: platform[] = [
+    {value: 'ehr', viewValue: 'EHR'},
+    {value: 'Physician Networking', viewValue: 'Physician Networking'},
+    {value: 'Medical Journal', viewValue: 'Medical Journal'},
+    {value: 'Telemedicine', viewValue: 'Telemedicine'}
+  ];
+
+  getSliderTickInterval(): number | 'auto' {
+    if (this.showTicks) {
+      return this.autoTicks ? 'auto' : this.tickInterval;
+    }
+
+    return 0;
+  }
+
+  somethingChanged() {
+    let earnings = this.registeredUsers * this.totalSlots;
+    this.circularTitle = '$' + earnings;
+    this.circularValue = (earnings / 1600000) * 100 ;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {ThemePalette} from '@angular/material/core';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
@@ -10,7 +26,38 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    FormsModule,
+    MatButtonToggleModule,
+    FlexLayoutModule,
+    MatProgressSpinnerModule,
+    NgCircleProgressModule.forRoot({
+      "radius": 60,
+      "space": -10,
+      "outerStrokeGradient": true,
+      "outerStrokeWidth": 10,
+      "outerStrokeColor": "#4882c2",
+      "outerStrokeGradientStopColor": "#53a9ff",
+      "innerStrokeColor": "#e7e8ea",
+      "innerStrokeWidth": 10,
+      "animateTitle": false,
+      "animationDuration": 1000,
+      "showUnits": false,
+      "showBackground": false,
+      "clockwise": false,
+      "startFromZero": false
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
